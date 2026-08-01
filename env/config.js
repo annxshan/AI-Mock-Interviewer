c7e981d8c3f6d2b0b861435059a77c6c8bc414b3
Anneshan Choudhury <choudhuryannneshan@gmail.com>
2026-08-01 22:55:53 +0530

    Delete env/config.js

--- env/config.js
+++
@@ -1,3 +0,0 @@
-// IMPORTANT: Replace with your actual Google AI Studio (Gemini) API Key.
-// This key should start with "AIza".
-export const API_KEY = "AQ.Ab8RN6KrX8ypZLNhj-tyCsa615YpBbdq0CVZba2IwoRR34ReQA";
